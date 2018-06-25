d3ac830397feecd3dc309afbd7cf1f50672b3b8a
libo <libokay@outlook.com>
2018-06-25 12:20:41 +0800

    首次提交

---
+++ developer/src/main/java/com/libok/androiddeveloper/activity/MainActivity.java
@@ -0,0 +1,29 @@
+package com.libok.androiddeveloper.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.libok.androiddeveloper.R;
+import com.xiaomi.market.sdk.UpdateResponse;
+import com.xiaomi.market.sdk.XiaomiUpdateAgent;
+import com.xiaomi.market.sdk.XiaomiUpdateListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        XiaomiUpdateAgent.update(this, true);
+        XiaomiUpdateAgent.setUpdateAutoPopup(true);
+        XiaomiUpdateAgent.setUpdateListener(new XiaomiUpdateListener() {
+            @Override
+            public void onUpdateReturned(int i, UpdateResponse updateResponse) {
+                Log.e(TAG, "onUpdateReturned: " + i + " " + updateResponse.versionCode);
+            }
+        });
+    }
+}
---
+++ developer/src/main/java/com/libok/androiddeveloper/view/BaseView.java
@@ -0,0 +1,63 @@
+package com.libok.androiddeveloper.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author liboK
+ * @date 2018/6/25 0025 11:34
+ * @e-mail libolf@outlook.com
+ * @description
+ */
+public class BaseView extends View {
+
+    private static final String TAG = "BaseView";
+
+    private Context mContext;
+    private Paint mPaint;
+
+    private int mWidth;
+    private int mHeight;
+    private PointF mCenterPoitnF;
+
+    public BaseView(Context context) {
+        this(context, null);
+    }
+
+    public BaseView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public BaseView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mContext = context;
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mCenterPoitnF = new PointF();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = getWidth();
+        mHeight = getHeight();
+        mCenterPoitnF.set(mWidth / 2F, mHeight / 2F);
+    }
+
+}
